e4bf9cc5605b58e002804b62441b9ab0a29cd12d
Ruijian Cao <cao7113@hotmail.com>
2020-04-30 16:13:08 +0800

    init commit

---
+++ lib/mixin_ruby_client.rb
@@ -0,0 +1,6 @@
+require "mixin_ruby_client/version"
+
+module MixinRubyClient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mixin_ruby_client/version.rb
@@ -0,0 +1,3 @@
+module MixinRubyClient
+  VERSION = "0.1.0"
+end
